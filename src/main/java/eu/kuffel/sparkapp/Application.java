5864afbd34d2dbee63623f82d5fab9f6a1f39d56
Kuffel <adam@kuffel.eu>
2017-01-06 01:47:56 +0100

    Updated Gradle file

---
+++ src/main/java/eu/kuffel/sparkapp/Application.java
@@ -0,0 +1,12 @@
+package eu.kuffel.sparkapp;
+
+/**
+ * Created by adam on 06.01.17.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Application started...");
+    }
+
+}
