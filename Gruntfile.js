a73d76dfafa0105ce177038dc68cf92f332ceac5
Kuffel <adam@kuffel.eu>
2017-01-23 16:12:56 +0100

    Added package.json, bower.json and Gruntfile.js to build process.

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+    module.exports = function (grunt) {
+
+        grunt.initConfig({
+            pkg : grunt.file.readJSON('package.json')
+
+        }); // END grunt.initConfig
+
+        grunt.registerTask('default', []);
+
+
+    }; // END module.exports = function (grunt)
